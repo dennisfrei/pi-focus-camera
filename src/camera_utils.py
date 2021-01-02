e60c9bbb7a3ddd9894cc1be71f78090cea262721
dennisfrei <dennisfrei@gmx.net>
2021-01-02 22:39:17 +0100

    changes

--- src/camera_utils.py
+++ src/camera_utils.py
@@ -18,3 +18,18 @@ class StreamingOutput(object):
                 self.condition.notify_all()
             self.buffer.seek(0)
         return self.buffer.write(buf)
+
+
+class StreamingOutputSimple(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            self.frame = self.buffer.getvalue()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
